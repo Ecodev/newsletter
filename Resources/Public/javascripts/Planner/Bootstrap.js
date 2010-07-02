6274e3d0d19358f755b3a8e543da7b9a86cc3760
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2010-07-02 07:50:16 +0000

    DEL obsolete/testing code
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/newsletter/trunk@35236 735d13b6-9817-0410-8766-e36946ffe9aa

--- Resources/Public/javascripts/Planner/Bootstrap.js
+++ Resources/Public/javascripts/Planner/Bootstrap.js
@@ -11,9 +11,6 @@ Ext.ns("TYPO3.Newsletter.Planner");
  */
 TYPO3.Newsletter.Planner.Bootstrap = Ext.apply(new TYPO3.Newsletter.Application.AbstractBootstrap, {
 	initialize: function() {
-//		this.addContentArea('management', 'F3-TYPO3-Management', {
-//			html: 'Management'
-//		});
 
 		/**
 		 * Handle a navigation token.
@@ -38,13 +35,6 @@ TYPO3.Newsletter.Planner.Bootstrap = Ext.apply(new TYPO3.Newsletter.Application.
 			component.setVisible(true);
 		});
 
-		// TODO refactor to helper method
-//		TYPO3.Newsletter.Application.on('TYPO3.Newsletter.UserInterface.SectionMenu.activated', function(itemId) {
-//			if (itemId === 'management') {
-//				Ext.History.add('management');
-//				Ext.getCmp('F3-TYPO3-UserInterface-center').getLayout().setActiveItem('F3-TYPO3-Management');
-//			}
-//		});
 	}
 });
 
