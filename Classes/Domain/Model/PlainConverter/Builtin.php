74c9203a469d5dc950939c4d992d7a802cbc542e
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2011-02-28 10:46:02 +0000

    make links unique in plain text converter
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/newsletter/trunk@44382 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Domain/Model/PlainConverter/Builtin.php
+++ Classes/Domain/Model/PlainConverter/Builtin.php
@@ -4,6 +4,8 @@ require_once(t3lib_extMgm::extPath('newsletter').'/3dparty/class.html2text.inc')
 
 class Tx_Newsletter_Domain_Model_PlainConverter_Builtin extends html2text implements Tx_Newsletter_Domain_Model_IPlainConverter
 {	
+	private $links = array();
+	
 	public function setContent($content, $contentUrl, $baseUrl)
 	{
 		$this->set_base_url($baseUrl);
@@ -14,5 +16,35 @@ class Tx_Newsletter_Domain_Model_PlainConverter_Builtin extends html2text implem
 	{	
 		return $this->get_text();
 	}
+	
+	function _convert()
+	{
+		$this->links = array();
+		return parent::_convert();
+	}
+	
+	/**
+	 * Override parent function to make links unique
+	 * @see 3dparty/html2text::_build_link_list()
+	 */
+	function _build_link_list( $link, $display )
+    {
+    	// If links already exists, return its existing reference
+    	if (array_key_exists($link, $this->links))
+    	{
+    		return $display . ' [' . $this->links[$link] . ']';
+    	}
+    	// Otherwise call the parent and memorize returned reference
+    	else
+    	{
+    		$result = parent::_build_link_list($link, $display);
+    		
+    		preg_match('/\[(\d+)\]$/', $result, $m);
+    		$reference = $m[1];
+    		
+    		$this->links[$link] = $reference;
+    		return $result;
+    	}
+    }
 }
 
