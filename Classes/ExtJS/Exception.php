6adb0fc47334908bf819f2ea7bcd6287ecf41d3b
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-03-18 10:30:21 +0000

    Removed no ExtDirect code
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mvc_extjs/trunk@45237 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/ExtJS/Exception.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Tx_MvcExtjs_ExtJS_Exception extends Exception {
-	
-}
-
-?>
--- ext_tables.php
+++ ext_tables.php
@@ -3,10 +3,10 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-if (TYPO3_MODE === 'BE') {
+/*if (TYPO3_MODE == 'BE') {
 	$TBE_MODULES['_dispatcher'][] = $TBE_MODULES['_dispatcher'][0];
 	$TBE_MODULES['_dispatcher'][0] = 'Tx_MvcExtjs_Dispatcher';
-}
+}*/
 
 //$GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['concatenateHandler'] = "EXT:mvc_extjs/Classes/PageRenderer/Service.php:&tx_MvcExtjs_PageRenderer_Service->doConcatenate";
 
