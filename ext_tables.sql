6991acd5f81fd4f9603a1052f054e01f073b9e5d
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2012-07-17 19:37:42 +0900

    Markers were not substitued if there were too many available fields #38586
    Recipient data were truncated in database resulting in undeserializable
    data and thus the markers were not substitued

--- ext_tables.sql
+++ ext_tables.sql
@@ -93,7 +93,7 @@ CREATE TABLE tx_newsletter_domain_model_email (
 	open_time int(11) unsigned DEFAULT '0' NOT NULL,
 	bounce_time int(11) unsigned DEFAULT '0' NOT NULL,
 	recipient_address varchar(255) DEFAULT '' NOT NULL,
-	recipient_data varchar(255) DEFAULT '' NOT NULL,
+	recipient_data text NOT NULL,
 	newsletter int(11) unsigned DEFAULT '0',
 	unsubscribed tinyint(1) unsigned DEFAULT '0' NOT NULL,
 
