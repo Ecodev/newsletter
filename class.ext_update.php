ec351fcd82181b8de2f46af949e9d83f5019dc94
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2015-02-13 10:34:54 +0900

    Provide migration path for records in DB
    We drop TC DirectMail data import, because that project was resuscitated and
    we cannot guarantuee it would work anymore.
    
    Instead we now provide a migration path for 2.2.3 and earlier to 2.3.0 and later

--- class.ext_update.php
+++ class.ext_update.php
@@ -24,184 +24,65 @@
  * ************************************************************* */
 
 /**
- * Class to import data from legacy tx_tcdirectmail
+ * Class to migrate DB records
  *
  * @package Newsletter
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
  */
 class ext_update
 {
-
     /**
-     * SQL queries to copy data from tcdirectmail to newsletter
+     * SQL queries to do migration
      * @var array
      */
     private $queries = array(
-        // Recipient lists
-        "INSERT INTO tx_newsletter_domain_model_recipientlist (
-			uid, pid, title, plain_only, lang, type, be_users, fe_groups, fe_pages, csv_url, csv_separator, csv_fields, csv_filename, csv_values, sql_statement, html_url, html_fetch_type, tstamp, crdate, deleted, hidden
-		) SELECT uid, pid, title, plain_only, lang,
-		CONCAT( '\Ecodev\Newsletter\Domain\Model\RecipientList_', CONCAT( UPPER( LEFT( REPLACE( targettype, 'tx_tcdirectmail_target_', '' ) , 1 ) ) , SUBSTRING( REPLACE( targettype, 'tx_tcdirectmail_target_', '' ) , 2 ) ) ),
-		beusers, fegroups, fepages, csvurl, csvseparator, csvfields, csvfilename, csvvalues, rawsql, htmlfile, htmlfetchtype, tstamp, crdate, deleted, hidden
-		FROM tx_tcdirectmail_targets;",
-        // Bounce accounts
-        "INSERT INTO tx_newsletter_domain_model_bounceaccount (
-			pid, email, server, protocol, username, password, tstamp, crdate, deleted, hidden
-		) SELECT pid, email, server, servertype, username, passwd, tstamp, crdate, deleted, hidden
-		FROM tx_tcdirectmail_bounceaccount;",
-        // Migrate newsletter from page and tx_tcdirectmail_lock to its own table tx_newsletter_domain_model_newsletter
-        "INSERT INTO tx_newsletter_domain_model_newsletter (
-			pid, planned_time, begin_time, end_time, recipient_list, repetition, sender_name, sender_email, plain_converter, attachments, inject_open_spy, inject_links_spy, bounce_account
-		) SELECT pages.uid, IF(tx_tcdirectmail_senttime, tx_tcdirectmail_senttime, tx_tcdirectmail_lock.begintime), tx_tcdirectmail_lock.begintime, tx_tcdirectmail_lock.stoptime, tx_tcdirectmail_real_target, tx_tcdirectmail_repeat, tx_tcdirectmail_sendername, tx_tcdirectmail_senderemail,
-		CONCAT( '\Ecodev\Newsletter\Domain\Model\PlainConverter\', CONCAT( UPPER( LEFT( REPLACE( tx_tcdirectmail_plainconvert, 'tx_tcdirectmail_plain_', '' ) , 1 ) ) , SUBSTRING( REPLACE( tx_tcdirectmail_plainconvert, 'tx_tcdirectmail_plain_', '' ) , 2 ) ) ),
-		tx_tcdirectmail_attachfiles, tx_tcdirectmail_spy, tx_tcdirectmail_register_clicks, tx_tcdirectmail_bounceaccount
-		FROM pages
-		INNER JOIN tx_tcdirectmail_lock ON (pages.uid = tx_tcdirectmail_lock.pid)
-		WHERE tx_tcdirectmail_real_target != 0;",
-        // Emails matching an existing newsletter
-        "INSERT INTO tx_newsletter_domain_model_email (
-			pid, begin_time, end_time, recipient_address, recipient_data, open_time, bounce_time, newsletter
-		) SELECT tx_tcdirectmail_sentlog.pid, begintime, sendtime, receiver, userdata, IF(beenthere, sendtime, 0), IF(bounced, sendtime, 0), tx_newsletter_domain_model_newsletter.uid
-		FROM tx_tcdirectmail_sentlog
-		INNER JOIN tx_newsletter_domain_model_newsletter ON (tx_tcdirectmail_sentlog.pid = tx_newsletter_domain_model_newsletter.pid AND tx_tcdirectmail_sentlog.begintime = tx_newsletter_domain_model_newsletter.begin_time);",
-        // Migrate CLI beuser
-        "INSERT INTO be_users (
-			pid, tstamp, username, password, admin, usergroup, disable, starttime, endtime, lang, email, db_mountpoints, options, crdate, cruser_id, realName, userMods, allowed_languages, uc, file_mountpoints, fileoper_perms, workspace_perms, lockToDomain, disableIPlock, deleted, TSconfig, lastlogin, createdByAction, usergroup_cached_list, workspace_id, workspace_preview
-		) SELECT pid, tstamp, REPLACE(username, 'tcdirectmail', 'newsletter') AS username, password, admin, usergroup, disable, starttime, endtime, lang, email, db_mountpoints, options, crdate, cruser_id, realName, userMods, allowed_languages, uc, file_mountpoints, fileoper_perms, workspace_perms, lockToDomain, disableIPlock, deleted, TSconfig, lastlogin, createdByAction, usergroup_cached_list, workspace_id, workspace_preview
-		FROM be_users
-		WHERE username = '_cli_tcdirectmail';",
-        "UPDATE fe_users SET tx_newsletter_bounce = tx_tcdirectmail_bounce WHERE tx_newsletter_bounce = 0;",
-        "UPDATE be_users SET tx_newsletter_bounce = tx_tcdirectmail_bounce WHERE tx_newsletter_bounce = 0;",
-        // Normalize case
-        "UPDATE tx_newsletter_domain_model_recipientlist SET type = '\Ecodev\Newsletter\Domain\Model\RecipientList_BeUsers' WHERE type = '\Ecodev\Newsletter\Domain\Model\RecipientList_Beusers';",
-        "UPDATE tx_newsletter_domain_model_recipientlist SET type = '\Ecodev\Newsletter\Domain\Model\RecipientList_FeGroups' WHERE type = '\Ecodev\Newsletter\Domain\Model\RecipientList_Fegroups';",
-        "UPDATE tx_newsletter_domain_model_recipientlist SET type = '\Ecodev\Newsletter\Domain\Model\RecipientList_FePages' WHERE type = '\Ecodev\Newsletter\Domain\Model\RecipientList_Fepages';",
-        "DELETE FROM tx_newsletter_domain_model_recipientlist WHERE type = '\Ecodev\Newsletter\Domain\Model\RecipientList_Ttaddress';", // ttaddress is not supported anymore
-        "UPDATE tx_newsletter_domain_model_recipientlist SET type = '\Ecodev\Newsletter\Domain\Model\RecipientList_Sql' WHERE type = '\Ecodev\Newsletter\Domain\Model\RecipientList_Rawsql';",
-        "UPDATE tx_newsletter_domain_model_recipientlist SET type = '\Ecodev\Newsletter\Domain\Model\RecipientList_CsvFile' WHERE type = '\Ecodev\Newsletter\Domain\Model\RecipientList_Csvfile';",
-        "UPDATE tx_newsletter_domain_model_recipientlist SET type = '\Ecodev\Newsletter\Domain\Model\RecipientList_CsvList' WHERE type = '\Ecodev\Newsletter\Domain\Model\RecipientList_Csvlist';",
-        "UPDATE tx_newsletter_domain_model_recipientlist SET type = '\Ecodev\Newsletter\Domain\Model\RecipientList_CsvUrl' WHERE type = '\Ecodev\Newsletter\Domain\Model\RecipientList_Csvurl';",
-        "UPDATE tx_newsletter_domain_model_recipientlist SET type = '\Ecodev\Newsletter\Domain\Model\RecipientList_Html' WHERE type = '\Ecodev\Newsletter\Domain\Model\RecipientList_html';",
-        // Convert tcdirectmail pages to standard pages
-        "UPDATE pages SET doktype = 1 WHERE doktype = 189;",
+        "UPDATE tx_scheduler_task SET serialized_task_object = REPLACE(serialized_task_object, 'O:29:\"Tx_Newsletter_Task_SendEmails\"', 'O:33:\"Ecodev\\\\Newsletter\\\\Task\\\\SendEmails\"');",
+        "UPDATE tx_newsletter_domain_model_recipientlist SET type = REPLACE(type, 'Tx_Newsletter_Domain_Model_RecipientList_', 'Ecodev\\\\Newsletter\\\\Domain\\\\Model\\\\RecipientList\\\\');",
+        "UPDATE tx_newsletter_domain_model_newsletter SET plain_converter = REPLACE(plain_converter, 'Tx_Newsletter_Domain_Model_PlainConverter_', 'Ecodev\\\\Newsletter\\\\Domain\\\\Model\\\\PlainConverter\\\\');",
     );
 
     /**
      * Main function, returning the HTML content of the module
-     * @global \TYPO3\CMS\Core\Database\DatabaseConnection $TYPO3_DB
-     * @return	string	HTML to display
+     * @return	string HTML to display
      */
     function main()
     {
         $content = '';
-        global $TYPO3_DB;
+        $content .= '<h2>Migration from Newsletter 2.2.3 to 2.3.0</h2>';
+        $content .= '<form name="migrateForm" action="" method ="post">';
+        $content .= '<p>Records in database from Newsletter 2.2.3, or earlier, will be migrated to 2.3.0, or later. This action is idempotent and can be repeated if necessary.</p>';
+        $content .= '<p><input type="submit" name="domigration" value ="Migrate" /></p>';
+        $content .= '</form>';
 
         // Action! Makes the necessary update
-        $update = \TYPO3\CMS\Core\Utility\GeneralUtility::_GP('importtcdirectmail');
+        $update = \TYPO3\CMS\Core\Utility\GeneralUtility::_GP('domigration');
 
-        // The update button was clicked
-        if (!empty($update) && $this->canImportFromTcdirectmail()) {
-            $content .= '<h2>Import successfull</h2>';
-
-            // Attempt to deactivate tcdirectmail via a URL loaded within iframe
-            if (\TYPO3\CMS\Core\Utility\GeneralUtility::_GP('deactivate')) {
-                $content .= '<p>Deactivated TCDirectmail.</p>';
-                $content .= '<iframe style="border: none; height: 0; width: 0;" src="/typo3/mod.php?M=tools_em&CMD[showExt]=tcdirectmail&CMD[remove]=1" width="0" height="0"></iframe>';
-            }
-
-            // Import data
-            $recordCount = $this->importFromTcdirectmail();
-            $newsletterCount = reset($TYPO3_DB->sql_fetch_row($TYPO3_DB->sql_query('SELECT COUNT(*) FROM tx_newsletter_domain_model_newsletter')));
-            $emailCount = reset($TYPO3_DB->sql_fetch_row($TYPO3_DB->sql_query('SELECT COUNT(*) FROM tx_newsletter_domain_model_email')));
-            $recipientListCount = reset($TYPO3_DB->sql_fetch_row($TYPO3_DB->sql_query('SELECT COUNT(*) FROM tx_newsletter_domain_model_recipientlist')));
-            $bounceAccountCount = reset($TYPO3_DB->sql_fetch_row($TYPO3_DB->sql_query('SELECT COUNT(*) FROM tx_newsletter_domain_model_bounceaccount')));
-
-            $content .= "<p>Successfully imported data from TCDirectmail:</p><ul>
-				<li>Newsletters: $newsletterCount</li>
-				<li>Emails: $emailCount</li>
-				<li>Recipient Lists: $recipientListCount</li>
-				<li>Bounce Accounts: $bounceAccountCount</li>
-				<li>Total records count: $recordCount</li>
-				</ul>";
-        } else {
-            $content .= '<h2>Import from TCDirectMail</h2>';
-
-            if ($this->canImportFromTcdirectmail()) {
-                $content .= '<form name="importForm" action="" method ="post">';
-                $content .= '<p>Import all data from TCDirectmail, including newsletter sent, to be send and emails (this will NOT import hyperlinks related data).</p>';
-                $content .= '<input type="checkbox" name="deactivate" id="deactivate" checked="checked" /><label for="deactivate">Attempt to deactivate TCDirectmail.</label>';
-                $content .= '<p><input type="submit" name="importtcdirectmail" value ="Import" /></p>';
-                $content .= '</form>';
-            } else {
-                $content .= '<p>Cannot import from TCDirectmail.</p><p>TCDirectmail tables not found, or Newsletter tables non-empty (already imported or started using it).</p>';
-            }
+        // The update button was clicked, migrate stuff
+        if (!empty($update)) {
+            $recordCount = $this->doMigration();
+            $content .= '<h3>Results</h3>';
+            $content .= "<p>$recordCount records successfully migrated.</p>";
         }
 
         return $content;
     }
 
     /**
-     * Returns whether an import from tcdirectmail is possible
-     * @global \TYPO3\CMS\Core\Database\DatabaseConnection $TYPO3_DB
-     * @return boolean
-     */
-    private function canImportFromTcdirectmail()
-    {
-        global $TYPO3_DB;
-
-        // Check that tcdirectmail tables exist
-        $requiredTables = array(
-            'tx_tcdirectmail_bounceaccount',
-            'tx_tcdirectmail_clicklinks',
-            'tx_tcdirectmail_lock',
-            'tx_tcdirectmail_sentlog',
-            'tx_tcdirectmail_targets',
-        );
-
-        $tables = array_keys($TYPO3_DB->admin_get_tables());
-        $missingTables = array_diff($requiredTables, $tables);
-
-        if (count($missingTables) != 0)
-            return false;
-
-        // Check that newsletter tables are empty otherwise we would have primary key collision
-        $emptyTables = array(
-            'tx_newsletter_domain_model_bounceaccount',
-            'tx_newsletter_domain_model_email',
-            'tx_newsletter_domain_model_recipientlist',
-            'tx_newsletter_domain_model_newsletter',
-        );
-
-        foreach ($emptyTables as $table) {
-            $res = $TYPO3_DB->sql_query("SELECT COUNT(*) AS count FROM $table");
-            $row = $TYPO3_DB->sql_fetch_row($res);
-            if ($row[0] != 0)
-                return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * Import data from tcdirectmail. Assume everything is available for import.
+     * Apply the migration
      * @global \TYPO3\CMS\Core\Database\DatabaseConnection $TYPO3_DB
      */
-    private function importFromTcdirectmail()
+    private function doMigration()
     {
         global $TYPO3_DB;
 
         $recordCount = 0;
         foreach ($this->queries as $query) {
             $res = $TYPO3_DB->sql_query($query);
-            if ($error = $TYPO3_DB->sql_error())
+            $error = $TYPO3_DB->sql_error();
+            if ($error) {
                 die("<pre>" . $query . "<br>" . $error . "</pre>");
+            }
             $recordCount += $TYPO3_DB->sql_affected_rows($res);
-        }
-
-        // Copy uploaded files from tcdirectmail directory to newsletter directory
-        foreach (glob(PATH_site . "uploads/tx_tcdirectmail/*") as $filename) {
-            $dest = str_replace('uploads/tx_tcdirectmail/', 'uploads/tx_newsletter/', $filename);
-            copy($filename, $dest);
         }
 
         return $recordCount;
@@ -210,7 +91,7 @@ class ext_update
     /**
      * This method checks whether it is necessary to display the UPDATE option at all
      *
-     * @param	string	$what: What should be updated
+     * @param string $what What should be updated
      */
     function access($what = 'all')
     {
